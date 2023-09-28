3879a6d6d6b79bfcb2849f79c996567a97406ccf
leminhtri1506 <115976210+leminhtri1506@users.noreply.github.com>
2023-09-28 18:18:47 +0700

    Add files via upload

---
+++ Bai2/Bai2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+
+namespace LAB4;
+
+public delegate void BalanceChangedEventHandler(decimal newBalance);
+
+public class Account
+{
+    private decimal balance;
+
+    public event BalanceChangedEventHandler BalanceChanged;
+
+    public decimal Balance
+    {
+        get { return balance; }
+        private set
+        {
+            if (balance != value)
+            {
+                balance = value;
+                OnBalanceChanged(balance);
+            }
+        }
+    }
+
+    public Account(decimal initialBalance)
+    {
+        balance = initialBalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+        }
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount > 0 && amount <= Balance)
+        {
+            Balance -= amount;
+        }
+    }
+
+    protected virtual void OnBalanceChanged(decimal newBalance)
+    {
+        BalanceChanged?.Invoke(newBalance);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Account account = new Account(1000);
+
+        // Register an event handler for the BalanceChanged event
+        account.BalanceChanged += (newBalance) =>
+        {
+            Console.WriteLine($"Balance changed. New balance: {newBalance:C}");
+        };
+
+        // Perform transactions to trigger the event
+        account.Deposit(500);
+        account.Withdraw(200);
+
+        // Unregister the event handler if no longer needed
+        account.BalanceChanged -= (newBalance) =>
+        {
+            Console.WriteLine($"Balance changed. New balance: {newBalance:C}");
+        };
+
+        Console.ReadLine();
+    }
+}
